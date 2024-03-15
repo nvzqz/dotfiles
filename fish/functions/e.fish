9254b8a498e1f319f2b90e2ff18f778ba844e5ab
Nikolai Vazquez <hello@nikolaivazquez.com>
2024-03-14 23:52:37 -0400

    Make `e` alias faster on macOS

--- fish/functions/e.fish
+++ fish/functions/e.fish
@@ -1,3 +1,11 @@
+# `code` is slow to run, so mimic it through `open`.
+if test "$EDITOR" = 'code' && test (uname -s) = Darwin
+    function e
+        open -a 'Visual Studio Code' -- $argv
+    end
+    return
+end
+
 function e --wraps='$EDITOR' --description 'open with "$EDITOR"'
     "$EDITOR" $argv
 end
